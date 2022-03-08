809f478c81d1e69217971793b6c26ff3a2b15e78
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-08 10:52:28 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,21 @@
+# a="python"
+# a[0]="P"
+
+# print(dir(str))
+
+# x="hELLo pyThon"
+# print(x.capitalize())
+
+# x="hEllOpYThon"
+# print(x.capitalize())
+
+# x="hEllo pyThon"
+# print(x.casefold())
+# print(x.lower())
+
+x="srinivas naidu"
+# print(len(x))
+# print(x.center)
+# print(x.center())
+print(x.center(25))
+print(x.center(25,'/'))
